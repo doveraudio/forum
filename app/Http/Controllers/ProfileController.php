d8022b3ba2b2ca3333a5ec1c74fe390c917ababc
Jeremy Leach <doveraudio@gmail.com>
2017-04-17 13:11:44 -0400

    small controller updates

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,17 +8,20 @@ class ProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         //
+        return Profile::all();
     }
 
     /**
      * Show the form for creating a new resource.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @return \Illuminate\Http\Response
      */
     public function create()
@@ -28,29 +31,45 @@ class ProfileController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $profile = Profile::create([
+        'user_id'       => $request->input('user_id'),
+        'image_id'      => $request->input('image_id'),
+        'headline'      => $request->input('headline'),
+        'description'   => $request->input('description'),
+        'birthdate'     => $request->input('birthdate'),
+        'location'      => $request->input('location'),
+        'signature'     => $request->input('signature')
+        ]);
+        $profile->save();
     }
 
     /**
      * Display the specified resource.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         //
+
+
+        return Profile::find($id);
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
@@ -61,7 +80,8 @@ class ProfileController extends Controller
 
     /**
      * Update the specified resource in storage.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -69,16 +89,32 @@ class ProfileController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $profile = Profile::find($id);
+        
+        $profile->user_id       = $request->input('user_id');
+        $profile->image_id      = $request->input('image_id');
+        $profile->headline      = $request->input('headline');
+        $profile->description   = $request->input('description');
+        $profile->birthdate     = $request->input('birthdate');
+        $profile->location      = $request->input('location');
+        $profile->signature     = $request->input('signature');
+        
+        $profile->save();
+
+
     }
 
     /**
      * Remove the specified resource from storage.
-     *
+     * Resource Properties:
+     * 'user_id','image_id','headline','description','birthdate','location','signature'
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+
+        Profile::delete($id);
     }
 }
